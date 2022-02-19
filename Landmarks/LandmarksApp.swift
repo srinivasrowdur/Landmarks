626aae84235ca99cc9dec77f1e9ffc363d9c028d
srinivasrowdur <99046278+srinivasrowdur@users.noreply.github.com>
2022-02-19 22:58:19 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Srinivas Rowdur on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
